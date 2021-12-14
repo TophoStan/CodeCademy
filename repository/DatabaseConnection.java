1f1328a7c22bb7fbcdd52195cb3c4d3ec1eb738c
TophoStan <saj.tophoven@student.avans.nl>
2021-12-14 14:55:08 +0100

    added databse connection

---
+++ repository/DatabaseConnection.java
@@ -0,0 +1,48 @@
+package repository;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * This program demonstrates how to establish database connection to Microsoft
+ * SQL Server.
+ * 
+ * @author www.codejava.net
+ *
+ */
+public class DatabaseConnection {
+
+    public void connect() {
+        Connection conn = null;
+
+        try {
+
+            String dbURL = "jdbc:sqlserver://aei-sql2.avans.nl\\studenten:1443;databaseName=CodeCademy7";
+            String user = "group7";
+            String pass = "groepje7";
+            conn = DriverManager.getConnection(dbURL, user, pass);
+            if (conn != null) {
+                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+
+}
