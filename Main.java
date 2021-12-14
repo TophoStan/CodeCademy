515adc6fdf8146708ccc1fe12daf0f33fe8159aa
sb.spanjers <sb.spanjers@gmail.com>
2021-12-14 16:13:56 +0100

    test

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import repository.*;
 
 public class Main {
     public static void main(String[] args) {
+        // database connection
         DatabaseConnection connection = new DatabaseConnection();
 
         connection.connect();
