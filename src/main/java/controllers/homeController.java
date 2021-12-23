1d351b1d7e272e24d5351373d7ee8ab86a6855a2
sb.spanjers <sb.spanjers@gmail.com>
2021-12-23 21:02:28 +0100

    Making Student edit page

--- src/main/java/controllers/homeController.java
+++ src/main/java/controllers/homeController.java
@@ -10,23 +10,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class switchController {
+public class homeController {
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void toHome(ActionEvent event) {
-        try {
-            Parent root = FXMLLoader.load(App.class.getResource("Home.fxml"));
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            System.out.println("hier");
-            e.printStackTrace();
-        }
-    }
     public void toStudent(ActionEvent event) {
         try {
             Parent root = FXMLLoader.load(App.class.getResource("StudentAdd.fxml"));
