19bab9040f4a51739f98b47e2236cced528834c9
TophoStan <saj.tophoven@student.avans.nl>
2021-12-23 16:17:31 +0100

    restructural changes

--- Gui/Run.java
+++
@@ -1,9 +0,0 @@
-package Gui;
-
-import javafx.application.Application;
-
-public class Run {
-    public static void main(String[] args) {
-        Application.launch(App.class);
-    }
-}
--- domain/Difficulty.java
+++
@@ -1,8 +0,0 @@
-package domain;
-
-public enum Difficulty {
-    EASY,
-    NORMAL,
-    HARD,
-    EXPERT
-}
