d77e81664a0defe4a2656bafc43ec3e7a7958c8d
TophoStan <saj.tophoven@student.avans.nl>
2021-12-24 11:53:41 +0100

    testing retrieving information

--- src/main/java/controllers/studentController.java
+++ src/main/java/controllers/studentController.java
@@ -2,10 +2,12 @@ package controllers;
 
 import com.example.codecademy.App;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +16,10 @@ public class studentController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML private TextField tfStudentAddName;
+    @FXML private TextField TfStudentAddEmail;
+    @FXML private TextField TFStudentAddGender;
+
 
     public void toHome(ActionEvent event) {
         try {
@@ -90,5 +96,10 @@ public class studentController {
 
     public void showStudentEditPlace(ActionEvent event) {
 
+    }
+
+
+    public void test(){
+        System.out.println(tfStudentAddName.getText());
     }
 }
