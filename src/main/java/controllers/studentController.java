defa3f5e29801c9ff4b64c82e0c3fc3af58b30d3
sb.spanjers <sb.spanjers@gmail.com>
2021-12-29 11:00:46 +0100

    resolving errors

--- src/main/java/controllers/studentController.java
+++ src/main/java/controllers/studentController.java
@@ -305,28 +305,6 @@ public class studentController {
             System.out.println(e);
         }
     }
-    public void deleteStudent(){
-        String email = tFStudentDeleteEmail.getText();
-        ArrayList<Student> studentsToDelete = new ArrayList<>();
-        if (checkEmail(email)) {
-            try {
-                databaseConnection.connect();
-                studentsToDelete = databaseConnection.retrieveStudents();
-                for (Student i : studentsToDelete) {
-                    if (i.getEmailAddress().equals(email)) {
-                        databaseConnection.deleteStudentFromDatabase(i);
-                        tFStudentDeleteEmail.setText("Student removed");
-                        showStudent();
-                        break;
-                    }
-                }
-            }catch (Exception e){
-                System.out.println(e);
-            }
-        } else {
-            tFStudentDeleteEmail.setText("Unknown email");
-        }
-    }
 
 
     public Date convertDate(int day, int month, int year) {
