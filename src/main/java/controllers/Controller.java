e069a3e0e2139d9c2b4871a1a1a7eb0ee2b0b7da
sb.spanjers <sb.spanjers@gmail.com>
2021-12-29 12:22:40 +0100

    implemented toPage method

--- src/main/java/controllers/Controller.java
+++ src/main/java/controllers/Controller.java
@@ -10,19 +10,20 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class homeController {
+public class Controller {
+
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void toStudent(ActionEvent event) {
+    public void toPage(ActionEvent event, String page) {
         try {
-            Parent root = FXMLLoader.load(App.class.getResource("StudentAdd.fxml"));
-            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            root = FXMLLoader.load(App.class.getResource(page + ".fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
---
+++ src/main/java/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+
+
+public class HomeController {
+
+    private Controller controller;
+
+    public void toStudent(ActionEvent event) {
+        controller.toPage(event, "StudentAdd");
+    }
+
+    public void toCourse(ActionEvent event) {
+        controller.toPage(event, "CourseAdd");
+    }
+
+    public void toEnrollment(ActionEvent event) {
+        controller.toPage(event, "EnrollmentAdd");
+    }
+
+    public void toContentItem(ActionEvent event) {
+        controller.toPage(event, "ContentItemAdd");
+    }
+}
